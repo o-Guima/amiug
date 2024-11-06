31488390938b5d9a74ce4bbdfc5b1bac0d43dd29
Joao Guilherme <jguilherme070105@gmail.com>
2024-11-05 21:08:55 -0300

    tamo junto

---
+++ WindowsApp2/WindowsApp2/Form1.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(238, 88)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(116, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.WindowsApp2.My.Resources.Resources.bb9b275aeaaf8300915c2c562d739166
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "2024"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ atividade 2 05 03/atividade 2 05 03/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double teste, S, Ns, Imp;
+            Console.WriteLine("1 - Imposto");
+            Console.WriteLine("2 - Novo Salario");
+            Console.WriteLine("3 - Classificação");
+            Console.WriteLine("4 - Finalizar");
+            Console.WriteLine("Digite a opção desejada");
+            teste = Convert.ToDouble(Console.ReadLine());
+            switch (teste)
+            {
+                case 1:
+                    Console.WriteLine("__Imposto__");
+                    Console.WriteLine("Digite o seu salario: ");
+                    S = Convert.ToDouble(Console.ReadLine());
+                    if (S < 500)
+                    {
+                        Imp = S * 0.05;
+
+                    }
+                    else if (S >= 500 && S <= 850)
+                    {
+                        Imp = S * 0.10;
+                        Console.WriteLine("O valor do imposto é de: " + Imp);
+                    }
+                    else
+                    {
+                        Imp = S * 0.15;
+                        Console.WriteLine("O valor do imposto é de: " + Imp);
+
+
+                    }
+                    break;
+                case 2:
+                    Console.WriteLine("__Novo Salario__");
+                    Console.WriteLine("Digite o seu salario: ");
+                    S = Convert.ToDouble(Console.ReadLine());
+                    if (S > 1500)
+                    {
+                        Ns = S + 25;
+                        Console.WriteLine("O valor do Novo salario é de: " + Ns);
+                    }
+                    else if (S >= 750 && S <= 1500)
+                    {
+                        Ns = S + 50;
+                        Console.WriteLine("O valor do Novo salario é de: " + Ns);
+                    }
+                    else if (S >= 450 && S <= 750)
+                    {
+                        Ns = S + 75;
+                        Console.WriteLine("O valor do Novo salario é de: " + Ns);
+                    }
+                    else
+                    {
+                        Ns = S + 100;
+                        Console.WriteLine("O valor do Novo salario é de: " + Ns);
+
+                    }
+                    break;
+                case 3:
+                    Console.WriteLine("__Classificação__");
+                    Console.WriteLine("Digite o seu salario: ");
+                    S = Convert.ToDouble(Console.ReadLine());
+                    if (S <= 700)
+                    {
+                        Console.WriteLine("O funcionario é Mal-remunerado");
+                    }
+                    else
+                        Console.WriteLine("O Funcionario é bem remunerado");
+                    Console.ReadKey();
+                    break;
+                case 4:
+                    Console.WriteLine("Você decidiu finalizar a consulta :D");
+                    break;
+
+
+
+
+
+
+
+            }
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
